9674bc664e8dd320db579b70bbbbf7d7216a0e74
Admin <Admin@DESKTOP-LBV1PJH>
2025-02-05 18:48:26 +0530

    Add project files.

---
+++ oop concepts/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+class company
+{
+
+    public void companyName()
+    {
+        Console.WriteLine("it will return company name");
+    }
+}
+
+
+class employee
+{
+
+    public string employeename ()
+    {
+        Console.WriteLine("enter your employeename ");
+        return Console.ReadLine();
+
+    }
+
+
+    //public int employeeage ()
+    //{
+    //    Console.WriteLine("age");
+    //     return Console.ReadLine();
+    //}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
